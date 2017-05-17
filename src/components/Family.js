5b4f4e4f4fe8ddfb5615e2a07e36325fca568b63
Thijs Wijnmaalen <thijs@wijnmaalen.name>
2017-05-17 16:02:13 +0200

    CRUD for family members

--- src/components/Family.js
+++ src/components/Family.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import Member from './Member';
 import './Family.css';
 
 class Family extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: null,
+      phone_number: null
+    }
+  }
+
   componentWillMount() {
     const { store } = this.props;
-    store.users.fetch();
+    store.family.fetch();
   }
 
   render() {
     const { store } = this.props;
-    const { users } = store;
+    const { family } = store;
 
-    if (users.isRequest('fetching')) {
+    if (family.isRequest('fetching')) {
       return (
         <p>Loading users ...</p>
       )
     }
+
+    const { state } = this;
 
     return (
       <div className="Family">
@@ -24,15 +35,11 @@ class Family extends Component {
         <h2 className="subtitle">Edit the people who get notified in case of panic</h2>
 
         <table className="table">
+          <colgroup width={100} />
+          <colgroup width={100} />
+          <colgroup width={100} />
           <tbody>
-            {users.models.map(user => {
-              return (
-                <tr>
-                  <td>{user.name}</td>
-                  <td>[edit actions]</td>
-                </tr>
-              )
-            })}
+            {family.models.map(member => <Member key={member.id} member={member} />)}
           </tbody>
         </table>
 
@@ -40,18 +47,22 @@ class Family extends Component {
           <div className="field">
             <label className="label">Name</label>
             <p className="control has-icons-left">
-              <input className="input" type="text" placeholder="Name"/>
+              <input className="input" type="text" placeholder="Name" value={state.name || ""} onChange={(e) => {
+                this.setState({ name: e.target.value });
+              }}/>
               <span className="icon is-small is-left">
                 <i className="fa fa-user"></i>
               </span>
             </p>
           </div>
           <div className="field">
-            <label className="label">E-mail address</label>
+            <label className="label">Phone number</label>
             <p className="control has-icons-left has-icons-right">
-              <input className="input" type="email" placeholder="Email"/>
+              <input className="input" type="email" value={state.phone_number || ""} onChange={(e) => {
+                this.setState({ phone_number: e.target.value });
+              }}/>
               <span className="icon is-small is-left">
-                <i className="fa fa-envelope"></i>
+                <i className="fa fa-phone"></i>
               </span>
               <span className="icon is-small is-right">
                 <i className="fa fa-check"></i>
@@ -60,7 +71,17 @@ class Family extends Component {
           </div>
           <div className="field">
             <p className="control">
-              <button className="button is-primary">Submit</button>
+              <button className="button is-primary" disabled={!state.name || !state.phone_number} onClick={() => {
+                const { name, phone_number } = state
+                family.create({
+                  name,
+                  phone_number
+                })
+                this.setState({
+                  name: null,
+                  phone_number: null
+                })
+              }}>Submit</button>
             </p>
           </div>
         </section>
