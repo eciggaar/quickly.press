c7db95a5cacc0be5888c388461feacc90fdfda85
Tom Offringa <tom.offringa@wearespindle.com>
2017-05-17 19:47:17 +0200

    Settingsicon

--- src/components/images/index.js
+++ src/components/images/index.js
@@ -2,10 +2,12 @@ import add_new from './add_new.svg';
 import family from './family.svg';
 import government from './government.svg';
 import logo from './logo';
+import settings from './settings.svg';
 
 export {
   add_new,
   family,
   government,
-  logo
+  logo,
+  settings
 }
