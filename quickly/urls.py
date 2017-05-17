83c08f45f08bff404b7309d28a94b53bb91579b1
Arjen Vellinga <arjen.vellinga@wearespindle.com>
2017-05-17 15:12:11 +0200

    WIP basic serializer

--- quickly/urls.py
+++ quickly/urls.py
@@ -3,7 +3,14 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.base import TemplateView
+from rest_framework.routers import DefaultRouter
 
+from quickly.schedules.api.view import SchedulesViewSet
+
+
+# Define routes, using the default router so the API is browsable.
+router = DefaultRouter()
+router.register(r'schedules', SchedulesViewSet)
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='spindle.html'), name='spindle'),
