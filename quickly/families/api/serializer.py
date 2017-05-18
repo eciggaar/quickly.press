bddf283f6937117e70344efa0712f73c6bd0bc06
Bob Voorneveld <info@bobvoorneveld.nl>
2017-05-18 11:41:28 +0200

    changed family serializer

--- quickly/families/api/serializer.py
+++ quickly/families/api/serializer.py
@@ -2,7 +2,7 @@ from quickly.families.models import FamilyMember
 from rest_framework import serializers
 
 
-class FamilySerializer(serializers.HyperlinkedModelSerializer):
+class FamilySerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
 
     class Meta:
